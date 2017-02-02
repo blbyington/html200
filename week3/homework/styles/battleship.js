50b9fd3a435d9a8f014bc1f6d89efa0b71f3c5ec
britt byington <blbyington@gmail.com>
2017-02-01 20:24:19 -0800

    initial

---
+++ week3/homework/styles/battleship.js
@@ -0,0 +1,35 @@
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+ 
+while (isSunk == false) {
+                guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+                if (guess < 0 || guess > 6) {
+                                alert("board a valid cell number!");
+                } else {
+                                guesses = guesses + 1;
+                                if (guess == location1 || guess == location2 || guess == location3) {
+                                                alert("HIT!");
+                                                hits = hits + 1;
+                                                if (hits == 3) {
+                                                                isSunk = true;
+                                                                alert("YAR!!! You sank me pirate ship!");
+                                                }
+                                } else {
+                                                alert("MISS");
+                                }
+                }
+}
+var stats = "You shot " + guesses + " cannon balls to sink me pirate ship, " +
+            "which means yer shootin' accuracy was " + (3/guesses);
+alert(stats);
+
+function reload() {
+    location.reload();
+}
+</script>
